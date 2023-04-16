c2e4dfb1e240b3cf7446dc9d816c7350b4820dbe
adian57011 <adian57011@gmail.com>
2023-04-16 22:02:34 +0600

    Dashboards For all usersgit add .

---
+++ Views/patientHome.php
@@ -0,0 +1,89 @@
+<html>
+    <head>
+        <title>Receptionist Home</title>
+
+        <style>
+            #index
+            {
+                position: fixed;
+                top: 25px;
+                right: 100px;
+
+                word-spacing: 20px;
+                font-size: 20px;
+                
+            }
+
+            #header
+            {
+                position: relative;
+                top: 25px;
+                left: 10px;
+            }
+
+            #feature-list
+            {
+                position: absolute;
+                top:100px;
+                left: 10px;
+
+                width: 250px;
+                height: 400px;
+
+                border:1px solid black;
+
+                line-height: 2.0;
+
+            }
+
+            #body-list
+            {
+                position: absolute;
+                top: 100px;
+                left:265px;
+
+                width: 1000px;
+                height: 400px;
+
+                border: 1px solid black;
+            }
+        </style>
+
+    </head>
+
+
+    <body>
+
+        <div id = "index">
+            <a href="">Home</a>
+            <a href="">Notification</a>
+            <a href="">Logout</a>
+        </div>
+        
+        <div id = "header">
+            <h1>X hospital Limited</h1>
+        </div>
+
+
+        <div id = "feature-list">
+            <ul>
+                <li> <a href=""> Records</a> </li>
+                <li> <a href="">Appointment Apply</a> </li>
+                
+            </ul>
+        </div>
+
+        <div id = "body-list">
+
+            <!--this is the main body of the program!! .. all the codes should start from here!!-->
+
+            <center>
+                <h2>Welcome Home </h2>
+            </center>
+
+        </div>
+        
+    </body>
+
+
+</html>
